e2d87568d6e408a2a704b97fef2da88d097916db
EIMIKI <kou.103.kan@gmail.com>
2019-11-19 03:12:08 +0900

    音声認識したテキストをpostされるハンドラを追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,10 +10,28 @@ import (
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
+
 	r.GET("/recognition", getRecognition)
+	r.POST("/postVoiceText", postVoiceText)
+
 	r.Run()
 }
 
 func getRecognition(c *gin.Context) {
 	c.HTML(http.StatusOK, "recognition.html", nil)
+}
+
+type Voice struct {
+	Text string `form:"text"`
+}
+
+func postVoiceText(c *gin.Context) {
+	voice := Voice{}
+
+	err := c.ShouldBind(&voice)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(voice.Text)
 }
