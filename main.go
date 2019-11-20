085f25624a62be15399acb762cb31ae2e38bf255
EIMIKI <kou.103.kan@gmail.com>
2019-11-20 17:35:49 +0900

    音声ファイルのロードを移動

--- main.go
+++ main.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"strings"
-
 	"talk-with-kiritan/config"
 	"talk-with-kiritan/controller"
 	"talk-with-kiritan/router"
 )
-
-var (
-	fileNames map[string]string //トリミングしたファイル名と元のファイル名
-)
-
-func init() {
-	extension := ".wav"
-	ignoreSymbols := []string{"。", "、", ",", ".", "・", "_", "＿", "!", "！", "?", "？", " ", "　", "…"}
-	fileNames = map[string]string{}
-
-	fmt.Println("Loading sound file ...")
-	files, err := ioutil.ReadDir("sounds")
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		fileName := file.Name()
-		trimmedFileName := strings.TrimRight(fileName, extension)
-		if trimmedFileName+extension == fileName { // 拡張子のバリデーション
-			for _, ignoreSymbol := range ignoreSymbols {
-				trimmedFileName = strings.ReplaceAll(trimmedFileName, ignoreSymbol, "")
-			}
-			fileNames[trimmedFileName] = fileName
-
-		}
-	}
-	fmt.Println("Sound file was Loaded!")
-}
 
 func main() {
 	config, err := config.GetConfig()
--- router/mainRouter.go
+++ router/mainRouter.go
@@ -1,1 +1,36 @@
 package router
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"talk-with-kiritan/config"
+)
+
+var (
+	fileNames map[string]string //トリミングしたファイル名と元のファイル名
+)
+
+func init() {
+	extension := ".wav"
+	ignoreSymbols := []string{"。", "、", ",", ".", "・", "_", "＿", "!", "！", "?", "？", " ", "　", "…"}
+	fileNames = map[string]string{}
+
+	fmt.Println("Loading sound file ...")
+	files, err := ioutil.ReadDir("sounds")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fileName := file.Name()
+		trimmedFileName := strings.TrimRight(fileName, extension)
+		if trimmedFileName+extension == fileName { // 拡張子のバリデーション
+			for _, ignoreSymbol := range ignoreSymbols {
+				trimmedFileName = strings.ReplaceAll(trimmedFileName, ignoreSymbol, "")
+			}
+			fileNames[trimmedFileName] = fileName
+
+		}
+	}
+	fmt.Println("Sound file was Loaded!")
+}
