eb0e28e470fa10521529e7c9438a669d9f892fff
EIMIKI <kou.103.kan@gmail.com>
2019-11-23 18:16:36 +0900

    keyに促音、長音を除いたファイル名を登録するように

--- router/mainRouter.go
+++ router/mainRouter.go
@@ -77,14 +77,24 @@ func getKeys(fileName string) ([]string, error) {
 		return nil, err
 	}
 
+	noSymbolFileName := ""
 	keys := []string{}
 	result = result.Next()
 	for ; !result.Next().IsZero(); result = result.Next() {
 		resultStruct := nodeToStruct(result)
+		if !resultStruct.isSymbol() { //記号を取り除いた元ファイル名の生成
+			noSymbolFileName += resultStruct.getSurface()
+		}
 		if resultStruct.allowAdd() {
 			keys = append(keys, resultStruct.getOrigin())
+		}
 	}
-	}
+
+	noSokuonFileName := trimSokuon(noSymbolFileName)
+	noLongVowelFileName := trimLongVowel(noSokuonFileName)
+	keys = append(keys, noSymbolFileName)
+	keys = append(keys, noSokuonFileName)
+	keys = append(keys, noLongVowelFileName)
 
 	return keys, nil
 }
@@ -110,6 +120,10 @@ func (t TaggerNode) getOrigin() string {
 	return t.Origin
 }
 
+func (t TaggerNode) getSurface() string {
+	return t.Surface
+}
+
 func nodeToStruct(node mecab.Node) TaggerNode {
 	ret := TaggerNode{}
 
@@ -121,4 +135,19 @@ func nodeToStruct(node mecab.Node) TaggerNode {
 	ret.Origin = splitedFeature[6]
 
 	return ret
+}
+
+func (t TaggerNode) isSymbol() bool {
+	if t.PartsOfSpeech == "記号" {
+		return true
+	}
+	return false
+}
+
+func trimLongVowel(fileName string) string {
+	return strings.ReplaceAll(fileName, "ー", "")
+}
+
+func trimSokuon(fileName string) string {
+	return strings.ReplaceAll(fileName, "っ", "")
 }
