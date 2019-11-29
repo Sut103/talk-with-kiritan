bce69e3482d107bba93cc473e1a76869b968ad82
EIMIKI <kou.103.kan@gmail.com>
2019-11-30 01:06:52 +0900

    入退出用ボイスを再生できるように

--- controller/discordController.go
+++ controller/discordController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
 	"talk-with-kiritan/config"
+	"time"
 )
 
 type DiscordController struct {
@@ -35,23 +36,41 @@ func (ctrl *DiscordController) MessageRecive(s *discordgo.Session, event *discor
 			}
 		}
 		if ctrl.VC != nil {
+			//参加メッセージ
 			s.ChannelMessageSend(discordChannel.ID, ctrl.Config.MessageJoin)
 
 			//音声ファイルのリクエスト受付を開始
 			go playAudioLoop(s, ctrl)
 
+			//　参加時Voice
+			if voice := ctrl.Config.AudioFileJoin; voice != "" {
+				time.Sleep(time.Second * 2)
+				dgvoice.PlayAudioFile(ctrl.VC, "sounds/"+voice, make(<-chan bool))
+				time.Sleep(time.Second * 2)
+			}
+
+			//音声ファイル名の送信を許可
 			ctrl.Main.VChs.Lock.Lock()
-			ctrl.Main.VChs.Condition = true //音声ファイル名の送信を許可
+			ctrl.Main.VChs.Condition = true
 			ctrl.Main.VChs.Lock.Unlock()
 		}
 	}
 
 	//VC退出
 	if event.Content == ctrl.Config.OrderMessageLeave {
+		//音声ファイル名の送信を遮断
 		ctrl.Main.VChs.Lock.Lock()
-		ctrl.Main.VChs.Condition = false //音声ファイル名の送信を遮断
+		ctrl.Main.VChs.Condition = false
 		ctrl.Main.VChs.Lock.Unlock()
 
+		//退出時Voice
+		if voice := ctrl.Config.AudioFileLeave; voice != "" {
+			time.Sleep(time.Second * 2)
+			dgvoice.PlayAudioFile(ctrl.VC, "sounds/"+voice, make(<-chan bool))
+			time.Sleep(time.Second * 2)
+		}
+
+		//退出メッセージ
 		s.ChannelMessageSend(discordChannel.ID, ctrl.Config.MessageLeave)
 		if err := ctrl.VC.Disconnect(); err != nil {
 			panic(err)
