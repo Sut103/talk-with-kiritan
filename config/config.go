181286d539169238a50b55c20b86964c5752c60d
EIMIKI <kou.103.kan@gmail.com>
2019-11-29 23:59:50 +0900

    config項目を追加

--- config/config.go
+++ config/config.go
@@ -5,11 +5,25 @@ import (
 	"io/ioutil"
 )
 
-type DiscordConfig struct {
-	Token string `json:"token"`
-}
 type Config struct {
-	Discord DiscordConfig `json:"session"`
+	Discord Discord `json:"discord"`
+	Server  Server  `json:"server"`
+}
+type Discord struct {
+	BotToken          string `json:"bot_token"`
+	OrderMessageJoin  string `json:"order_message_join"`
+	OrderMessageLeave string `json:"order_message_leave"`
+	MessageJoin       string `json:"message_join"`
+	MessageLeave      string `json:"message_leave"`
+}
+type Users struct {
+	User string `json:"user"`
+	Pass string `json:"pass"`
+}
+type Server struct {
+	AudioFileExtension string  `json:"audio_file_extension"`
+	Idle               int     `json:"idle"`
+	Users              []Users `json:"users"`
 }
 
 func GetConfig() (Config, error) {
